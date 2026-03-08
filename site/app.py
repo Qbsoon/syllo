326ad625e99a16a13b499a7c2c7d8a89a5ad7820
qbsoon <ramsters110@gmail.com>
2026-03-08 02:34:38 +0000

    Added file processing capabilities

--- site/app.py
+++ site/app.py
@@ -1,4 +1,4 @@
-# Imports
+# --- Imports ---
 import os
 from quart import Quart, render_template, request, jsonify, send_from_directory
 from quart_rate_limiter import RateLimiter, rate_limit
@@ -7,53 +7,99 @@ import urllib.parse
 import requests
 import httpx
 import uvicorn
+from datetime import datetime
+import pandas as pd
+from werkzeug.utils import secure_filename
 
 
-# App definition
+# --- 'Static' values ---
+LOCAL_MODELS = ['qwen3_14b_q5km']
+REMOTE_MODELS = []
+UPLOAD_ARCHIVE = "uploads"
+LCPP_URL = "http://127.0.0.1:51791/v1/chat/completions"
+
+
+# --- App definition ---
 app = Quart(__name__, template_folder="pages")
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
 RateLimiter(app)
 
 
-# Main site
+# --- Main site ---
 @app.route("/")
 async def index():
     return await render_template("index.html")
 
 
-# Ask AI endpoint
-@app.route("/dologic", methods=["POST"])
-@rate_limit(1, timedelta(seconds=3))
-async def do_logic():
-    data = await request.get_json()
-
-    syll = urllib.parse.unquote(data.get("syll", ""))
-    stype = urllib.parse.unquote(data.get("type", ""))
-
-    url = "http://127.0.0.1:51791/v1/chat/completions"
-    reply = 'Something went wrong'
+# --- Process types/models function ---
+async def figure_out(model, stype, syll, conc = ''):
+    reply = 'Something went wrong.'
 
     messages = ""
     if stype == 0 or stype == '0':
         messages = [{'role': 'system', 'content': 'You are a professor of logic and your job is to evaluate syllogisms. A - All x are y; E - No x are y; I - Some x are y; O - Some x are not y. Check if they\'re logically correct and explain their reasoning. Common checks:\n5 Rules (Classical Method)\n1. Middle Term Distribution (M): A middle term must be distributed (appear as the predicate of a general proposition or the subject of a negative proposition) in at least one premise.\n2. Avoiding Two Negative Premises: A valid conclusion cannot be drawn from two negative premises (e.g., "Some S are not P" and "No S is P").\n3. Negative Consistency: A negative premise implies a negative conclusion (if there is a negative premise, the conclusion must be negative).\n4. Assertion Consistency: Two affirmative premises (e.g., "Every A is B") imply an affirmative conclusion.\n5. Decomposition of Terms in the Conclusion: If a term is distributed in the conclusion, it must also be distributed in the premise from which it originates (e.g., S or P). Syllogisms can have more than 2 premises. Sometimes a name can appear using its synonyms. Premises do not have to be in order.'}, {'role': 'user', 'content': f'Check this syllogism:\n{syll}\nProvide a brief, not long of a response!\nDO NOT REFER TO RULES BY THEIR NUMBERS/IDS!'}]
     elif stype == 1 or stype == '1':
         messages =  [{'role': 'system', 'content': 'You are a professor of logic and your job is to evaluate syllogisms. A - All x are y; E - No x are y; I - Some x are y; O - Some x are not y. Check if they\'re logically correct and explain their reasoning. Common checks:\n5 Rules (Classical Method)\n1. Middle Term Distribution (M): A middle term must be distributed (appear as the predicate of a general proposition or the subject of a negative proposition) in at least one premise.\n2. Avoiding Two Negative Premises: A valid conclusion cannot be drawn from two negative premises (e.g., "Some S are not P" and "No S is P").\n3. Negative Consistency: A negative premise implies a negative conclusion (if there is a negative premise, the conclusion must be negative).\n4. Assertion Consistency: Two affirmative premises (e.g., "Every A is B") imply an affirmative conclusion.\n5. Decomposition of Terms in the Conclusion: If a term is distributed in the conclusion, it must also be distributed in the premise from which it originates (e.g., S or P). Syllogisms can have more than 2 premises. Sometimes a name can appear using its synonyms. Premises do not have to be in order.'}, {'role': 'user', 'content': f'Check these syllogism premises and make a conclusion based on them:\n{syll}\nProvide a brief, not long of a response!\nDO NOT REFER TO RULES BY THEIR NUMBERS/IDS!'}]
+    else:
+        reply += " Wrong action type."
 
-    reply = local_prompt(messages)
+    if (model in LOCAL_MODELS):
+        reply = await local_prompt(messages)
+    elif (model in REMOTE_MODELS):
+        reply = await remote_prompt(messages, model)
+    else:
+        reply += " Wrong model."
+
+    return reply
+
+
+# --- Ask AI endpoint ---
+@app.route("/api/dologic", methods=["POST"])
+@rate_limit(1, timedelta(seconds=3))
+async def do_logic():
+    data = await request.get_json()
+
+    syll = urllib.parse.unquote(data.get("syll", ""))
+    stype = urllib.parse.unquote(data.get("type", ""))
+    model = urllib.parse.unquote(data.get("model", ""))
+
+    reply = await figure_out(model, stype, syll)
 
     return jsonify({"result": reply})
 
 
-# Local AI function
+# -- Process file endpoint ---
+@app.route("/api/domorelogic", methods=["POST"])
+@rate_limit(1, timedelta(seconds=3))
+async def do_more_logic():
+    data = await request.get_json()
+
+    file = urllib.parse.unquote(data.get("filePass", ""))
+    stype = urllib.parse.unquote(data.get("type", ""))
+    model = urllib.parse.unquote(data.get("model", ""))
+
+    syllos = pd.read_csv("uploads/" + file)
+    syllos['response'] = ''
+
+    for i, syllo in syllos.iterrows():
+        reply = await figure_out(model, stype, syllo['premises'], syllo['conclusion'])
+        syllos.at[i, 'response'] = reply
+    
+    syllos.to_csv("uploads/" + file)
+    return jsonify({"success": True})
+
+
+# --- Local AI function ---
 async def local_prompt(messages):
     async with httpx.AsyncClient() as client:
-        response = await client.post(url, json={'messages': messages, 'max_tokens': -1}, timeout=240.0)
+        response = await client.post(LCPP_URL, json={'messages': messages, 'max_tokens': -1}, timeout=240.0)
     reply = response.json()['choices'][0]['message']['content']
     if "</think>" in reply:
         reply = reply.split("</think>")[1].strip()
     return reply
 
 
-# Remote AI function
+# --- Remote AI function ---
 ## TODO: ADAM
 ## Otrzymuje tablicę obiektów json: [{'role': 'system', 'content': 'system_prompt'}, {'role': 'user', 'content': 'user_prompt'}] i nazwę/identyfikator modelu
 ## Odpytuje wybrany model groq i zwraca odpowiedź
@@ -66,7 +112,7 @@ async def remote_prompt(messages, model):
     return reply
 
 
-# Generate syllogism function
+# --- Generate syllogism function ---
 ## TODO: Martin
 ## Otrzymuje: num - liczba sylogizmów, sampling - zawartość sampling file, min_a, max_a - liczby tak jak w generatorze
 ## Katalog biblioteki, w którym są te jej wymagane dwa pliki .py i podkatalog 'history'
@@ -84,7 +130,7 @@ async def generate_syllo(num, sampling, min_a, max_a):
     return result
 
 
-# Access to files
+# --- Access to files ---
 @app.route("/scripts/<filename>")
 async def serve_scripts(filename):
     return await send_from_directory(os.path.join(".", "scripts"), filename)
@@ -93,12 +139,55 @@ async def serve_scripts(filename):
 async def serve_styles(filename):
     return await send_from_directory(os.path.join(".", "styles"), filename)
 
+@app.route("/uploads/<filename>")
+async def server_download(filename):
+    return await send_from_directory(os.path.join(".", "uploads"), filename)
 
-# Run
+
+# Upload section
+@app.route('/api/upload-syllos', methods=['POST'])
+async def upload_file():
+
+	files = await request.files
+	if 'sylloFile' not in files:
+		return jsonify({"success": False, "error": "No syllogism file part in the request"}), 400
+	file = files['sylloFile']
+
+	if file.filename == '':
+		return jsonify({"success": False, "error": "No selected file"}), 400
+
+	allowed_extensions = ['csv']
+	original_filename = secure_filename(file.filename)
+	file_ext = original_filename.rsplit('.', 1)[1].lower() if '.' in original_filename else ''
+
+	if not file_ext or file_ext not in allowed_extensions:
+		return jsonify({"success": False, "error": "Invalid file type. Only CSV allowed."}), 400
+
+	os.makedirs(UPLOAD_ARCHIVE, exist_ok=True)
+
+	new_filename = f"upload_{datetime.now().strftime("%Y%m%d_%H%M%S")}.{file_ext}"
+
+	upload_filepath = os.path.join(UPLOAD_ARCHIVE, new_filename)
+
+	try:
+		await file.save(upload_filepath)
+		return jsonify({"success": True, "filename": new_filename})
+	except Exception as e:
+		return jsonify({"success": False, "error": "Server error while saving the file."}), 500
+	
+
+# --- Error Handlers ---
+@app.errorhandler(413)
+async def request_entity_too_large(error):
+	app.logger.warning(f"Upload failed: File too large (413). Limit is {app.config.get('MAX_CONTENT_LENGTH') / (1024*1024)}MB.")
+	return jsonify(success=False, error="File is too large. Please upload a file smaller than {}MB.".format(app.config.get('MAX_CONTENT_LENGTH') // (1024*1024)), limit=app.config.get('MAX_CONTENT_LENGTH') // (1024*1024)), 413
+
+
+# --- Run ---
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
-        host=os.environ.get("MAIN_ADDR", "0.0.0.0"),
+        host=os.environ.get("MAIN_ADDR"),
         port=51790,
         workers=4,
     )
--- site/scripts/main.js
+++ site/scripts/main.js
@@ -1,11 +1,36 @@
 (function () {
     // Sekcja pobierania elementów
+    //Buttons
     const sendBtn = document.getElementById("send");
+    const uploadBtn = document.getElementById("syllUpload");
+    const modeTextBtn = document.getElementById('modeText');
+    const modeFileBtn = document.getElementById('modeFile');
+    //Inputs
     const syllEl = document.getElementById("syll");
     const typeEl = document.getElementById("type");
+    const modelEl = document.getElementById("model");
+    const uploadDialog = document.getElementById("uploadDialog");
+    const syllFile = document.getElementById("syllFile");
+    //Output
     const resultEl = document.getElementById("result");
     const spinner = document.getElementById("spinner");
     const statusText = document.getElementById("status-text");
+    const resultFile = document.getElementById("resultFile");
+
+    // Przełączniki Tekst/Plik
+    modeTextBtn.addEventListener('click', () => {
+        document.querySelectorAll('.type-one').forEach(el => el.classList.remove('hidden'));
+        document.querySelectorAll('.type-file').forEach(el => el.classList.add('hidden'));
+        modeTextBtn.classList.add('active');
+        modeFileBtn.classList.remove('active');
+    });
+
+    modeFileBtn.addEventListener('click', () => {
+        document.querySelectorAll('.type-one').forEach(el => el.classList.add('hidden'));
+        document.querySelectorAll('.type-file').forEach(el => el.classList.remove('hidden'));
+        modeTextBtn.classList.remove('active');
+        modeFileBtn.classList.add('active');
+    });
 
     // Sekcja logiki zapytania
     let cooldown = false;
@@ -16,48 +41,232 @@
             return;
         }
 
-        const syll = encodeURIComponent(syllEl.value);
-        const type = encodeURIComponent(typeEl.value);
+        if (modeTextBtn.classList.contains('active')) {
+            const syll = encodeURIComponent(syllEl.value);
+            const type = encodeURIComponent(typeEl.value);
+            const model = encodeURIComponent(modelEl.value);
 
-        spinner.classList.remove("hidden");
-        let elapsed = 0;
-        statusText.textContent = " Przetwarzanie... 0.0s";
-        const timer = setInterval(() => {
-            elapsed += 100;
-            statusText.textContent = ` Przetwarzanie... ${(elapsed / 1000).toFixed(1)}s`;
-        }, 100);
+            spinner.classList.remove("hidden");
+            let elapsed = 0;
+            statusText.textContent = " Przetwarzanie... 0.0s";
+            const timer = setInterval(() => {
+                elapsed += 100;
+                statusText.textContent = ` Przetwarzanie... ${(elapsed / 1000).toFixed(1)}s`;
+            }, 100);
 
-        sendBtn.disabled = true;
-        cooldown = true;
+            sendBtn.disabled = true;
+            cooldown = true;
 
-        try {
-            const res = await fetch("/dologic", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ syll, type }),
-            });
+            try {
+                const res = await fetch("/api/dologic", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ syll, type, model }),
+                });
 
-            clearInterval(timer);
-            spinner.classList.add("hidden");
+                clearInterval(timer);
+                spinner.classList.add("hidden");
 
-            if (res.status === 429) {
-                statusEl.textContent = "Ograniczenie przepustowości - poczekaj 3 sekundy.";
-            } else if (res.ok) {
-                const data = await res.json();
-                resultEl.value = data.result;
-                statusText.textContent = `Czas: ${(elapsed / 1000).toFixed(1)}s`;
-            } else {
-                statusText.textContent = "Error: " + res.statusText;
+                if (res.status === 429) {
+                    statusEl.textContent = "Ograniczenie przepustowości - poczekaj 3 sekundy.";
+                } else if (res.ok) {
+                    const data = await res.json();
+                    resultEl.value = data.result;
+                    statusText.textContent = `Czas: ${(elapsed / 1000).toFixed(1)}s`;
+                } else {
+                    statusText.textContent = "Error: " + res.statusText;
+                }
+            } catch (err) {
+                clearInterval(timer);
+                spinner.classList.add("hidden");
+                statusEl.textContent = "Network error.";
             }
-        } catch (err) {
-            clearInterval(timer);
-            spinner.classList.add("hidden");
-            statusEl.textContent = "Network error.";
+
+            setTimeout(() => {
+                cooldown = false;
+                sendBtn.disabled = false;
+            }, 3000);
+        }
+        else if (modeFileBtn.classList.contains('active')) {
+            const filename = syllFile.dataset.value;
+            if (filename != '') {
+                const filePass = encodeURIComponent(filename);
+                const type = encodeURIComponent(typeEl.value);
+                const model = encodeURIComponent(modelEl.value);
+
+                spinner.classList.remove("hidden");
+                let elapsed = 0;
+                statusText.textContent = " Przetwarzanie... 0.0s";
+                const timer = setInterval(() => {
+                    elapsed += 100;
+                    statusText.textContent = ` Przetwarzanie... ${(elapsed / 1000).toFixed(1)}s`;
+                }, 100);
+
+                sendBtn.disabled = true;
+                cooldown = true;
+                
+                try {
+                    const response = await fetch('/api/domorelogic', {
+                        method: 'POST',
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({ filePass, type, model }),
+                    });
+
+                    clearInterval(timer);
+                    spinner.classList.add("hidden");
+
+                    if (response.status === 429) {
+                        statusEl.textContent = "Ograniczenie przepustowości - poczekaj 3 sekundy.";
+                    }
+
+                    const result = await response.json()
+                    if (result.success) {
+                        resultFile.download = filename;
+                        resultFile.href = `uploads/${filename}`;
+                        resultFile.parentElement.classList.remove('hidden');
+                    }
+                    else {
+                        console.log('Processing file failed');
+                        throw new Error(procResult.error || 'Processing file failed.');
+                    }
+                } catch (err) {
+                    clearInterval(timer);
+                    spinner.classList.add("hidden");
+                    statusEl.textContent = "Network error.";
+                }
+
+                setTimeout(() => {
+                    cooldown = false;
+                    sendBtn.disabled = false;
+                }, 3000);
+            }
+        }
+    });
+
+    uploadBtn.addEventListener("click", async () => {
+        uploadDialog.click();
+    })
+
+    uploadDialog.addEventListener('change', async (event) => {
+        const file = event.target.files[0];
+        if (!file) {
+            console.log("No file selected.");
+            return;
         }
 
-        setTimeout(() => {
-            cooldown = false;
-            sendBtn.disabled = false;
-        }, 3000);
+        const allowedTypes = ['text/csv', 'application/vnd.ms-excel', 'text/plain'];
+        if (!allowedTypes.includes(file.type.toLowerCase())) {
+            alert("Nieprawidłowy typ pliku");
+            uploadDialog.value = '';
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('sylloFile', file);
+
+        try {
+            resultFile.download = ''
+            resultFile.href = ''
+
+            const response = await fetch('/api/upload-syllos', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.status === 413) {
+                const errorData = await response.json();
+                alert(errorData.error || `Przekroczono limit rozmiaru: ${errorData.limit} MB.`);
+                return;
+            }
+
+            if (!response.ok) {
+                const errorData = await response.json().catch(() => ({ error: 'Server error during upload.' }));
+                throw new Error(errorData.error || `Upload failed with status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            if (result.success && result.filename) {
+                if (syllFile) {
+                    syllFile.value = file.name;
+                    syllFile.value = file.name;
+                    syllFile.dataset.value = result.filename;
+                    resultFile.download = '';
+                    resultFile.href = '';
+                    resultFile.parentElement.classList.add('hidden');
+                }
+            } else {
+                throw new Error(result.error || 'Upload failed: No filename returned.');
+            }
+        } catch (error) {
+            console.error('Error uploading file:', error);
+            alert(`Błąd wgrywania pliku: ${error.message}`);
+        } finally {
+            uploadDialog.value = '';
+        }
     });
+
+    syllFile.addEventListener('dragover',  (e) => {
+        e.preventDefault();
+        syllFile.classList.add('dragover');
+    });
+
+    syllFile.addEventListener('dragleave', () => {
+        syllFile.classList.remove('dragover');
+    });
+
+    syllFile.addEventListener('drop', async (e) => {
+        e.preventDefault();
+        syllFile.classList.remove('dragover');
+        const file = e.dataTransfer.files[0];
+
+        const allowedTypes = ['text/csv', 'application/vnd.ms-excel', 'text/plain'];
+        if (!allowedTypes.includes(file.type.toLowerCase())) {
+            alert("Nieprawidłowy typ pliku");
+            uploadDialog.value = '';
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('sylloFile', file);
+
+        try {
+            const response = await fetch('/api/upload-syllos', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.status === 413) {
+                const errorData = await response.json();
+                alert(errorData.error || `Przekroczono limit rozmiaru: ${errorData.limit} MB.`);
+                return;
+            }
+
+            if (!response.ok) {
+                const errorData = await response.json().catch(() => ({ error: 'Server error during upload.' }));
+                throw new Error(errorData.error || `Upload failed with status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            if (result.success && result.filename) {
+                if (syllFile) {
+                    syllFile.value = file.name;
+                    syllFile.dataset.value = result.filename;
+                    resultFile.download = '';
+                    resultFile.href = '';
+                    resultFile.parentElement.classList.add('hidden');
+                }
+            } else {
+                throw new Error(result.error || 'Upload failed: No filename returned.');
+            }
+        } catch (error) {
+            console.error('Error uploading file:', error);
+            alert(`Błąd wgrywania pliku: ${error.message}`);
+        } finally {
+            uploadDialog.value = '';
+        }
+    });
+
+    new MutationObserver(() => {
+        resultFile.parentElement.classList.toggle('hidden', resultFile.download == '');
+    }).observe(resultFile, { attributes: true, attributeFilter: ['href'] });
 })();
